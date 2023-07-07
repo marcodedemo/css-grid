755f028b834f7938edffcac70ef6ce7a06814cd8
marcodedemo <marco.dedemo@gmail.com>
2023-07-07 16:49:29 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+
+
+<script setup>
+
+</script>
+
+
+
+
+
+
+<template>
+
+
+<div class="container">
+
+  <div class="box yellow">1</div>
+  <div class="box purple">2</div>
+  <div class="box pink">3</div>
+  <div class="box blue">4</div>
+
+  
+</div>
+
+
+
+
+</template>
+
+
+
+
+
+
+<style scoped>
+
+
+.container{
+
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 100px 300px 100px 100px;
+  gap: 20px;
+
+
+
+  width: 1000px;
+
+}
+
+.yellow{
+  background-color: #daa521;
+  grid-column: 1 / -1;
+}
+
+.purple{
+  background-color: #800080;
+  grid-row: 2 / 4;
+}
+
+.pink{
+  background-color: #ee82ee;
+}
+
+.blue{
+  background-color: #aed8e6;
+  grid-column: 1 / -1;
+}
+
+
+
+</style>
